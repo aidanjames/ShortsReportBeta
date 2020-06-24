54aa23cd9606006029fb654efaf8b6fbe6018e05
AidanJP <aidanpendlebury@gmail.com>
2020-06-24 19:37:06 +0100

    Initial Commit

---
+++ ShortsReportBeta/ShortsReportBetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShortsReportBetaApp.swift
+//  ShortsReportBeta
+//
+//  Created by Aidan Pendlebury on 24/06/2020.
+//
+
+import SwiftUI
+
+@main
+struct ShortsReportBetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
